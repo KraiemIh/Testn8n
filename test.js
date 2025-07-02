6ed75c51b8be98964d11b21ada22e4867ccc332a
KraiemIh <iheb.kraiem18@gmail.com>
2025-07-02 22:52:12 +0100

    modify 4

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const add = require('./index');
 
-if (add(1, 2) !== 3) {
-  throw new Error("Test failed: 1 + 2 should be 3"); // volontairement faux
+if (add(1, 2) !== 4) {
+  throw new Error("Test failed: 1 + 2 should be 4"); // volontairement faux
 }
